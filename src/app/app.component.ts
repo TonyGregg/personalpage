efa3da89b9fa5a706565cd1cbb2177b38944e065
Unknown <antonygenil@gmail.com>
2018-04-30 05:24:26 -0400

    Auto stash before merge of "master" and "personanpage/master"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  //backgroundImage = require('../images/header-bg.jpg');
+  getBakgroundImg() {
+    return "url('../images/header-bg.jpg')";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather/weather.component';
+import { QuoteComponent } from './quote/quote/quote.component';
+import { PersonComponent } from './person/person/person.component';
+import { StockSummaryComponent } from './stocks/stock-summary/stock-summary.component';
+import { NewsComponent } from './news/news/news.component';
+import { SportsNewsComponent } from './news/sports-news/sports-news.component';
+import { MusicComponent } from './music/music/music.component';
+import { ListComponent } from './todo/list/list.component';
+import { CalendarComponent } from './calendar/calendar/calendar.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    QuoteComponent,
+    PersonComponent,
+    StockSummaryComponent,
+    NewsComponent,
+    SportsNewsComponent,
+    MusicComponent,
+    ListComponent,
+    CalendarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
